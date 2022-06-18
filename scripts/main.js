37c317fdf76cc88332975029e447014b96246c18
camilod17 <kdodino1999@gmail.com>
2022-06-18 16:04:53 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,61 @@
+///PROYECTOS///
+
+$('.buttonProject-1').click(function(){
+    urlProject1='http://camilow.pythonanywhere.com/'
+    window.open(urlProject1, '_blank'); 
+})
+
+$('.buttonProject-2').click(function(){
+    urlProject2='https://model-agency.netlify.app/'
+    window.open(urlProject2, '_blank'); 
+})
+$('.buttonProject-3').click(function(){
+    alert("Este proyecto aún se encuentra en fase de diseño. ¡Vuelva Pronto! :)")
+})
+///REDES SOCIALES///
+let linkedin = document.querySelector("#linkedin")
+let github = document.querySelector("#github")
+let twitter = document.querySelector("#twitter")
+let gmail = document.querySelector("#gmail")
+
+$(linkedin).click(function () {
+    window.open("https://www.linkedin.com/in/camilo-hernandez-dodino/", '_blank')
+});
+$(github).click(function () {
+    window.open("https://github.com/CamiloD17", '_blank')
+});
+$(twitter).click(function () {
+    window.open("https://twitter.com/CamiloDodino17", '_blank')
+});
+$(gmail).click(function () {
+    window.open("mailto:kdodino1999@gmail.com", '_blank')
+});
+
+///FORMULARIO CON LA LIBRERIA FORMSPREE///
+const $form = document.querySelector("#form")
+
+$form.addEventListener("submit",handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action,{
+        method: this.method,
+        body: form,
+        headers:{
+            "Accept" : "application/json"
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert("Su mensaje ha sido enviado con éxito, pronto me pondré en contácto contigo.")
+    }else{
+        alert("Ha habido un problema al enviar el mensaje")
+    }
+}
+
+
+
+
+
+
---
+++ scripts/scrolls.js
@@ -0,0 +1,71 @@
+ScrollReveal().reveal('.name', { 
+    duration: 3000,
+    origin: 'top',
+    distance: '-50%'
+});
+ScrollReveal().reveal('.description', { 
+    duration: 6000,
+    origin: 'top',
+    distance: '-50%'
+});
+ScrollReveal().reveal('.btn-hireMe', { 
+    duration: 6000,
+    origin: 'left',
+    distance: '50%'
+});
+ScrollReveal().reveal('.cv', { 
+    duration: 3000,
+    origin: 'left',
+    distance: '50%'
+});
+ScrollReveal().reveal('.logos', { 
+    duration: 10000,
+    origin: 'left',
+    distance: '50%'
+});
+
+ScrollReveal().reveal('.img-1', { 
+    duration: 3000,
+    origin: 'top',
+    distance: '-50%',
+});
+ScrollReveal().reveal('.experience', { 
+    duration: 3000,
+    origin: 'top',
+    distance: '-50%',
+});
+ScrollReveal().reveal('.project-1', { 
+    duration: 3500,
+    origin: 'top',
+    distance: '-50%',
+});
+ScrollReveal().reveal('.project-2', { 
+    duration: 4000,
+    origin: 'top',
+    distance: '-50%',
+});
+ScrollReveal().reveal('.project-3', { 
+    duration: 4500,
+    origin: 'top',
+    distance: '-50%',
+});
+ScrollReveal().reveal('.containerText-4', { 
+    duration: 2000,
+    origin: 'right',
+    distance: '-50%',
+});
+ScrollReveal().reveal('.imgContactMe', { 
+    duration: 2000,
+    origin: 'right',
+    distance: '50%',
+});
+ScrollReveal().reveal('.contactMe', { 
+    duration: 2000,
+    origin: 'right',
+    distance: '-50%',
+});
+ScrollReveal().reveal('.logos-social', { 
+    duration: 6000,
+    origin: 'left',
+    distance: '50%'
+});
